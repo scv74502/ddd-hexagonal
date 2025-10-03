32553363a36e45a476cb9a6f1e047eed914f1018
kunwoo park <scv74502@gmail.com>
2025-10-04 01:22:46 +0900

    first commit

---
+++ src/main/kotlin/org/example/splearn/SplearnApplication.kt
@@ -0,0 +1,11 @@
+package org.example.splearn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SplearnApplication
+
+fun main(args: Array<String>) {
+    runApplication<SplearnApplication>(*args)
+}
